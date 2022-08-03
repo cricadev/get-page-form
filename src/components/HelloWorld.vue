b17e4a8bb1337cfbd888b67855445a2d23b7afe0
CricaDev <cricadev@Cristians-MacBook-Pro.local>
2022-08-03 10:32:11 -0500

    new content

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,15 +7,18 @@ defineProps({
 const text = ref("");
 const type = ref("");
 const check = ref(false);
-const count = ref(0);
+const country = ref("");
+function test() {
+  console.log(country.value);
+}
 function handleInput() {
   return check.value ? "thank-you" : "";
 }
 function handleSubmit() {
   window.open(
-    `https://staging--medallia-regional-staging.netlify.app/${type.value}/${
-      text.value
-    }/${handleInput()}`
+    `https://staging--medallia-regional-staging.netlify.app/${country.value
+      .toLowerCase()
+      .trim()}/${type.value.trim()}/${text.value.trim()}/${handleInput()}`
   );
 }
 </script>
@@ -23,6 +26,18 @@ function handleSubmit() {
 <template>
   <h1 class="py-6 text-6xl text-white">{{ msg }}</h1>
   <form @submit.prevent="handleSubmit" class="flex flex-col gap-4 text-center">
+    <label for="country">Country:</label>
+
+    <select name="country" id="country" v-model="country" @change="test">
+      <option value="BR">BRAZIL</option>
+      <option value="LATAM">LATAM</option>
+      <option value="ES">SPAIN</option>
+      <option value="KR">KOREAN</option>
+      <option value="JP">JAPAN</option>
+      <option value="FR">FRANCE</option>
+      <option value="IT">ITALY</option>
+      <option value="DE">GERMANY</option>
+    </select>
     <label for="type">Type:</label>
     <input
       id="type"
@@ -48,7 +63,10 @@ function handleSubmit() {
       v-model="check"
       @input="handleInput"
     />
-    <button type="submit" class="w-1/3 py-4 mx-auto bg-green-400">
+    <button
+      type="submit"
+      class="w-1/3 py-4 mx-auto bg-green-400 hover:bg-green-400/75 active:bg-green-400/25"
+    >
       Submit
     </button>
   </form>
---
+++ src/plugins/link-resolver.js
@@ -0,0 +1,572 @@
+export default function (doc) {
+  if (doc.isBroken) {
+    return "/not-found";
+  }
+  //Platform pages
+
+  if (doc.id === "YBr0CBUAACMAif27") {
+    return "/platform/";
+  }
+  if (doc.id === "YBww4BUAACYAj4aJ") {
+    return "/platform/action-intelligence/";
+  }
+  if (doc.id === "Yh5sABIAACEA6YJP") {
+    return "/platform/act/";
+  }
+  if (doc.id === "YBw3ORUAACMAj6Mj") {
+    return "/platform/admin-suite/";
+  }
+  if (doc.id === "YhftnRIAACAAzPHK") {
+    return "/platform/analytics/";
+  }
+  if (doc.id === "YCG_aBUAACQAp-5S") {
+    return "/platform/api/";
+  }
+  if (doc.id === "YBw-iRUAACMAj8OD") {
+    return "/platform/ask-now/";
+  }
+  if (doc.id === "YBszdRUAACUAixeS") {
+    return "/platform/athena/";
+  }
+  if (doc.id === "YBxImRUAACUAj_BZ") {
+    return "/platform/conversations/";
+  }
+  if (doc.id === "YBxdEBUAACQAkEp5") {
+    return "/platform/cx-journeys/";
+  }
+  if (doc.id === "Yfw0hBAAACQAcbD7") {
+    return "/platform/decibel-by-medallia/";
+  }
+  if (doc.id === "YBxjkBUAACYAkGX9") {
+    return "/platform/employee-ideas/";
+  }
+  if (doc.id === "YBxroBUAACUAkIog") {
+    return "/platform/feedback-management/";
+  }
+  if (doc.id === "YBxvdhUAACUAkJtU") {
+    return "/platform/ideas/";
+  }
+  if (doc.id === "YBxl0RUAACQAkHAa") {
+    return "/platform/insights-suite/";
+  }
+  if (doc.id === "YBt21hUAACMAjEIa") {
+    return "/platform/integration-layer/";
+  }
+  if (doc.id === "YCIgABUAACQAqZmF") {
+    return "/platform/medallia-digital-anywhere/";
+  }
+  if (doc.id === "YBx1DRUAACYAkLRM") {
+    return "/platform/messaging/";
+  }
+  if (doc.id === "YCJIGxUAACYAqkua") {
+    return "/platform/mobile/";
+  }
+  if (doc.id === "YBt2jRUAACMAjEDO") {
+    return "/platform/organizational-hierarchy-management/";
+  }
+  if (doc.id === "YfCD7xAAACQAPsRw") {
+    return "/platform/signals/";
+  }
+  if (doc.id === "YCIlwhUAACUAqbL5") {
+    return "/platform/social/";
+  }
+  if (doc.id === "YAn-9xEAACAA6ofI") {
+    return "/platform/speech/";
+  }
+  if (doc.id === "YCI1lRUAACMAqfk7") {
+    return "/platform/text-analytics/";
+  }
+  if (doc.id === "YgFUURAAACQAh913") {
+    return "/platform/total-experience-profiles/";
+  }
+  if (doc.id === "YCItaxUAACUAqdTP") {
+    return "/platform/video/";
+  }
+  if (doc.id === "YCJO0RUAACQAqmnE") {
+    return "/platform/voices/";
+  }
+
+  //Solution Pages
+  if (doc.id === "YC3qaREAACkA5GOG") {
+    return "/solutions/account-management/";
+  }
+  if (doc.id === "YC6vrhUAACoAfAUZ") {
+    return "/solutions/automotive/";
+  }
+  if (doc.id === "YEBrChAAACUALmf4") {
+    return "/solutions/covid-19/";
+  }
+  if (doc.id === "YCzFSBIAACIAbJmL") {
+    return "/solutions/cx-professionals/";
+  }
+  if (doc.id === "YPiDVRAAACIAsa3l") {
+    return "/solutions/digital-professionals";
+  }
+  if (doc.id === "Yf1p1RAAAB8Adw2v") {
+    return "/solutions/digital/";
+  }
+  if (doc.id === "Yfq8sRAAACAAazPf") {
+    return "/solutions/digital-suite/";
+  }
+  if (doc.id === "YC3aVBEAACkA5Bxv") {
+    return "/solutions/ex-professionals/";
+  }
+  if (doc.id === "YfA8HRAAACAAPYU9") {
+    return "/solutions/ex-professionals2/";
+  }
+  if (doc.id === "Ytm1sBAAACYA1Mxx") {
+    return "/solutions/energy-utilities/";
+  }
+  if (doc.id === "YuRwNBIAACIAHTZK") {
+    return "/solutions/financial-services/";
+  }
+  if (doc.id === "YXCdOhIAACEAMxob") {
+    return "/solutions/government/";
+  }
+  if (doc.id === "YDtE0RAAACEAF9X6") {
+    return "/solutions/government/take-care/";
+  }
+  if (doc.id === "YVSIoBAAACIAyUpc") {
+    return "/solutions/government/uk/";
+  }
+  if (doc.id === "YuG-yhIAACIAEUsy") {
+    return "/solutions/healthcare/";
+  }
+  if (doc.id === "YfA8HRAAACAAPYU9") {
+    return "/human-resources-professionals/";
+  }
+  if (doc.id === "YPdYRhAAAGdkrEna") {
+    return "/solutions/government/higher-education/";
+  }
+  if (doc.id === "YDYU5RAAACEA0Ga0") {
+    return "/solutions/insurance/";
+  }
+  if (doc.id === "YFRZ5BAAACgAFdOs") {
+    return "/solutions/life-sciences/";
+  }
+  if (doc.id === "YC4hGxEAACkA5VP1") {
+    return "/solutions/location-based-operations/";
+  }
+  if (doc.id === "YDaxjRAAACIA0v_A") {
+    return "/solutions/manufacturing/";
+  }
+  if (doc.id === "YDiHbBMAACgAKK4Z") {
+    return "/solutions/nonprofit/";
+  }
+  if (doc.id === "YC4ohBEAAHOk5XMs") {
+    return "/solutions/research-insights/";
+  }
+  if (doc.id === "YDszMBAAACUAF4gi") {
+    return "/solutions/restaurants-food-service/";
+  }
+  if (doc.id === "Yt6jUhAAACUA6pfS") {
+    return "/solutions/retail/";
+  }
+  if (doc.id === "Yth8dBAAACYAz1fe") {
+    return "/solutions/technology-services/";
+  }
+  if (doc.id === "YD0ttBAAACMAIES9") {
+    return "/solutions/telecommunications-media/";
+  }
+  if (doc.id === "YryMORAAACkAsGhI") {
+    return "/solutions/travel-hospitality/";
+  }
+
+  //Legal Pages
+  if (doc.id === "YDtcGxAAACIAGDyj") {
+    return "/legal-terms-medallia-android-mobile-app/";
+  }
+  if (doc.id === "YEAevBAAACUALReM") {
+    return "/nda-and-activity-on-medallia-premises/";
+  }
+  if (doc.id === "YDtewhAAACIAGEhx") {
+    return "/terms-of-use/";
+  }
+  if (doc.id === "YDtaERAAACEAGDOe") {
+    return "/code-of-business-conduct-and-ethics-for-partners-and-suppliers/";
+  }
+  if (doc.id === "YDtdHRAAACUAGEEd") {
+    return "/privacy-policy/japan/";
+  }
+  if (doc.id === "YDtdeRAAACUAGEK2") {
+    return "/purchase-order-terms-and-conditions/";
+  }
+  if (doc.id === "YDtdzhAAACIAGEQy") {
+    return "/reporting-application-surveys/";
+  }
+  if (doc.id === "YDteRhAAACIAGEZE") {
+    return "/sustainability/";
+  }
+  //Single Pages
+
+  if (doc.id === "YBj1pRUAACMAgQvI") {
+    return "/about-us/";
+  }
+  if (doc.id === "YBuBmxUAACYAjHGr") {
+    return "/b2b-customer-experience/";
+  }
+  if (doc.id === "YBuBgRUAACQAjHE1") {
+    return "/b2c-customer-experience/";
+  }
+  if (doc.id === "YEkHLBAAACIAVAc0") {
+    return "/bestpractices/";
+  }
+  if (doc.id === "YEphFBAAACUAWakt") {
+    return "/canada/";
+  }
+  if (doc.id === "YDtaERAAACEAGDOe") {
+    return "/cem-software/";
+  }
+  if (doc.id === "YEKO_BAAACIAN_TN") {
+    return "/code-of-business-conduct-and-ethics-for-partners-and-suppliers/";
+  }
+  if (doc.id === "YiEBChIAACEA9O7V") {
+    return "/community/";
+  }
+  if (doc.id === "YFEJAxQAACYAoD9H") {
+    return "/company-values/";
+  }
+  if (doc.id === "Yf12XxAAACQAd0a7") {
+    return "/contact-centers/";
+  }
+  if (doc.id === "YD3EYBAAACMAIsnQ") {
+    return "/contact-us/";
+  }
+  if (doc.id === "YEKUjhAAACUAOA2Q") {
+    return "/customer-experience-management/";
+  }
+  if (doc.id === "YEaFAhAAACUASW3B") {
+    return "/customer-experience-management-training/";
+  }
+  if (doc.id === "YFmKpxAAACkALMGj") {
+    return "/customer-retention/";
+  }
+  if (doc.id === "YD1CVBAAACEAIIev") {
+    return "/customers/";
+  }
+  if (doc.id === "YEniiRAAACIAV7l-") {
+    return "/data-protection/";
+  }
+  if (doc.id === "YGTKcxAAACkAXfWs") {
+    return "/demo/";
+  }
+  if (doc.id === "YXmiYxUAACkAh4KJ") {
+    return "/diversity/";
+  }
+  if (doc.id === "YendQBAAACQAIVg0") {
+    return "/employee-experience/";
+  }
+  if (doc.id === "YFrOwhAAACgAMiqr") {
+    return "/experts-on-demand/";
+  }
+  if (doc.id === "YGuG_BAAACkAe5tC") {
+    return "/gartner-report/";
+  }
+  if (doc.id === "YG4n0xAAACgAh0dT") {
+    return "/giving/";
+  }
+  if (doc.id === "YEaBVhAAACUASV1E") {
+    return "/institute/";
+  }
+  if (doc.id === "YkdVtxUAACYAsfEO") {
+    return "/isv-medallia-gartner-report/";
+  }
+  if (doc.id === "YEAI3BAAACIALLak") {
+    return "/leadership/";
+  }
+  if (doc.id === "YEnbKhAAACMAV5hG") {
+    return "/learn-more/";
+  }
+  if (doc.id === "YDtcGxAAACIAGDyj") {
+    return "/legal-terms-medallia-android-mobile-app/";
+  }
+  if (doc.id === "YFw_NxAAACoAOIni") {
+    return "/love/";
+  }
+  if (doc.id === "YBhCsRUAACMAffEQ") {
+    return "/mastercast/";
+  }
+  if (doc.id === "YEqOLRAAACIAWnI-") {
+    return "/medallia-experience-cloud-core-product-description/";
+  }
+  if (doc.id === "YFwFERAAACoAN4ni") {
+    return "/medallia-racing/";
+  }
+  if (doc.id === "YEcdnhAAACMATBAz") {
+    return "/medallia-travel-expense-guidelines/";
+  }
+  if (doc.id === "YGK2ARAAACoAVPXF") {
+    return "/mxm-powered-by-medallia-experience-cloud/";
+  }
+  if (doc.id === "YEAevBAAACUALReM") {
+    return "/nda-and-activity-on-medallia-premises/";
+  }
+  if (doc.id === "YFwuOBAAACgAOD7b") {
+    return "/net-promoter-score/";
+  }
+  if (doc.id === "YGUiLRAAACsAX2uI") {
+    return "/newsroom/";
+  }
+  if (doc.id === "YEaLRxAAACUASYm_") {
+    return "/ocem-assessment/";
+  }
+  if (doc.id === "YkdYNRUAACYAsfw1") {
+    return "/oracle-medallia-cx-guide/";
+  }
+  if (doc.id === "YHX0YBAAACoAqdDy") {
+    return "/partner-request/";
+  }
+  if (doc.id === "YQBOQxEAACQA8W-8") {
+    return "/partners/";
+  }
+  if (doc.id === "YgwFGhAAAB8AJTNo") {
+    return "/partners/adobe";
+  }
+  if (doc.id === "YQJW5REAACIA-oqb") {
+    return "/partners/become-a-partner/";
+  }
+
+  if (doc.id === "YHThUhAAACsApQPV") {
+    return "/partners/search/";
+  }
+  if (doc.id === "YBiPlxUAACQAf0eY") {
+    return "/powerup/";
+  }
+  if (doc.id === "YDtdHRAAACUAGEEd") {
+    return "/privacy-policy/japan/";
+  }
+  if (doc.id === "YEqUZhAAACIAWo3K") {
+    return "/privacy-policy-archive/";
+  }
+  if (doc.id === "YEqSghAAACEAWoV1") {
+    return "/privacy-policy/privacy-shield-notice/";
+  }
+  if (doc.id === "YEqY4BAAACUAWqHU") {
+    return "/privacy-policy/";
+  }
+  if (doc.id === "YBuBxBUAACUAjHJn") {
+    return "/product-experience/";
+  }
+  if (doc.id === "YiaCZBIAACAADRFD") {
+    return "/products/";
+  }
+  if (doc.id === "YDtdeRAAACUAGEK2") {
+    return "/purchase-order-terms-and-conditions/";
+  }
+  if (doc.id === "YDtdzhAAACIAGEQy") {
+    return "/reporting-application-surveys/";
+  }
+  if (doc.id === "YEh32hAAACIAUcFM") {
+    return "/resource-content/5-essential-ways-to-use-the-voice-of-customer-in-your-game-plan/";
+  }
+  if (doc.id === "YHK-WRAAACgAm5oB") {
+    return "/resource-content/5-ways-experience-signals-are-shaping-the-future-of-cx/";
+  }
+  if (doc.id === "YGKL_RAAACoAVDsc") {
+    return "/resource-content/5-ways-to-use-the-voice-of-employee-in-reimagining-the-workplace-of-tomorrow";
+  }
+  if (doc.id === "YRQs8BIAACIAS1Mw") {
+    return "/partners/resellers/";
+  }
+  if (doc.id === "YElSexAAACMAVVX_") {
+    return "/partners/salesforce/";
+  }
+  if (doc.id === "Ye8cGhAAACAAOH8O") {
+    return "/race-equality-index/";
+  }
+  if (doc.id === "YBt29BUAACYAjEKi") {
+    return "/security/";
+  }
+  if (doc.id === "YFrXnRAAACoAMlG-") {
+    return "/services/";
+  }
+  if (doc.id === "YFwj3BAAACkAOBFG") {
+    return "/sponsorships/";
+  }
+  if (doc.id === "YFlnzhAAACsALC41") {
+    return "/sponsorships/36th-americas-cup/";
+  }
+  if (doc.id === "YOdMkBEAAB8AxzmD") {
+    return "/sponsorships/perry-baker/";
+  }
+
+  if (doc.id === "YFlkNxAAACkALB5v") {
+    return "/sponsorships/breezyjohnson/";
+  }
+  if (doc.id === "YFleBBAAACoALAL7") {
+    return "/sponsorships/medallia-x-usa-rugby/";
+  }
+  if (doc.id === "YEfnfxAAACUAT1h_") {
+    return "/sponsorships/sarahhauser/";
+  }
+  if (doc.id === "YDteRhAAACIAGEZE") {
+    return "/sustainability/";
+  }
+  if (doc.id === "YGZNUhAAACgAZJA1") {
+    return "/third-party-software-attribution/";
+  }
+  if (doc.id === "YDtewhAAACIAGEhx") {
+    return "/terms-of-use/";
+  }
+  if (doc.id === "YEcgJRAAACUATBrD") {
+    return "/voice-of-the-customer/";
+  }
+  if (doc.id === "YGJdoBAAACkAU2_X") {
+    return "/what-is-employee-experience/";
+  }
+  if (doc.id === "YEdRYhAAACEATOV5") {
+    return "/xchange/";
+  }
+  if (doc.id === "YksoYRUAACQAwsB8") {
+    return "/zingle-abm/";
+  }
+  if (doc.id === "YoJ47xEAAB8As-dT") {
+    return "/zingle-abm/?mt=ihg";
+  }
+
+  //Medallia Go pages
+  if (doc.id === "YSfblhAAACIA1p60") {
+    return "/medallia-go/";
+  }
+  if (doc.id === "YUJbohIAACMAOjdc") {
+    return "/medallia-go/banks/";
+  }
+  if (doc.id === "YUI0VxIAACIAOYRF") {
+    return "/medallia-go/credit-unions/";
+  }
+  if (doc.id === "YUJqsRIAACEAOnvm") {
+    return "/medallia-go/healthcare/";
+  }
+  if (doc.id === "YUIVGhIAACMAOPpp") {
+    return "/medallia-go/hospitality/";
+  }
+  if (doc.id === "YUJaLxIAACAAOjDF") {
+    return "/medallia-go/property-management";
+  }
+
+  //Crowdicity Pages
+  if (doc.id === "YWcSlxIAACIACjiz") {
+    return "/crowdicity/";
+  }
+  if (doc.id === "YXDywhIAACMANHDg") {
+    return "/crowdicity/product/";
+  }
+  if (doc.id === "YXGtVhIAACIAN5t9") {
+    return "/crowdicity/product/discover/";
+  }
+  if (doc.id === "YXG0VBIAACEAN7sj") {
+    return "/crowdicity/product/discover/idea-management/";
+  }
+  if (doc.id === "YXG9AxIAACAAN-Hs") {
+    return "/crowdicity/product/discover/challenge-management/";
+  }
+  if (doc.id === "YXHFohIAACEAOAie") {
+    return "/crowdicity/product/discover/analytics/";
+  }
+  if (doc.id === "YXHGkBIAACAAOAwh") {
+    return "/crowdicity/product/discover/mobile/";
+  }
+  if (doc.id === "YYBetBUAACgApQFL") {
+    return "/crowdicity/product/grow/";
+  }
+  if (doc.id === "YYBlUBUAACgApR7f") {
+    return "/crowdicity/product/grow/crowd-management/";
+  }
+  if (doc.id === "YYBsaRUAACgApT6g") {
+    return "/crowdicity/product/grow/user-tools/";
+  }
+  if (doc.id === "YYBs4hUAACsApUC_") {
+    return "/crowdicity/product/grow/gamification/";
+  }
+  if (doc.id === "YYBtIBUAACkApUHf") {
+    return "/crowdicity/product/grow/powerful-moderation";
+  }
+  if (doc.id === "YYGwOxUAACgAqgnP") {
+    return "/crowdicity/product/launch/";
+  }
+  if (doc.id === "YYGzdhUAACkAqhf2") {
+    return "/crowdicity/product/launch/no-technical-knowhow-required/";
+  }
+  if (doc.id === "YYG0yRUAACgAqh3Q") {
+    return "/crowdicity/product/launch/connect-integrate/";
+  }
+  if (doc.id === "YYG1FhUAACgAqh8j") {
+    return "/crowdicity/product/launch/trusted-secure/";
+  }
+  if (doc.id === "YXD_OBIAACEANKfn") {
+    return "/crowdicity/uses/";
+  }
+  if (doc.id === "YYG8XxUAACkAqj-_") {
+    return "/crowdicity/sectors/brands-and-consumer/";
+  }
+  if (doc.id === "YYKo_BUAACkArkFC") {
+    return "/crowdicity/sectors/legal-and-financial/";
+  }
+  if (doc.id === "YYKpQRUAACgArkKG") {
+    return "/crowdicity/sectors/technology-media-telecoms/";
+  }
+  if (doc.id === "YYKpgBUAACkArkOb") {
+    return "/crowdicity/sectors/construction-engineering-utilities/";
+  }
+  if (doc.id === "YYKpmhUAACoArkQd") {
+    return "/crowdicity/sectors/government/";
+  }
+  if (doc.id === "YYKp_RUAACgArkXn") {
+    return "/crowdicity/sectors/education-and-research/";
+  }
+  if (doc.id === "YYKqIxUAACkArkai") {
+    return "/crowdicity/sectors/healthcare/";
+  }
+  if (doc.id === "YYKqQxUAACsArkdA") {
+    return "/crowdicity/sectors/charity-and-ngos/";
+  }
+
+  if (doc.id === "YIekvhEAACMAuQDP") {
+    return "/impact/";
+  }
+  if (doc.id === "YIsQvhEAACMAx3BT") {
+    return "/impact/letter-from-ceo/";
+  }
+  if (doc.id === "YNTadBMAACAAAavK") {
+    return "/facebook/";
+  }
+  if (doc.id === "YNo9hxEAACMASIsh") {
+    return "/facebook/why-medallia/";
+  }
+  if (doc.id === "Yo0DqxEAACYAw3FM") {
+    return "/thunderhead/";
+  }
+  if (doc.id === "YrOBmxEAACQADAsG") {
+    return "/adobe-reseller-deal-registration/";
+  }
+
+  if (doc.id === "YtHuIBEAACIAA6ey") {
+    return "/experience-now/city-tour/";
+  }
+  if (doc.id === "YtBOdREAACAA_GZN") {
+    return "/experience-now/";
+  }
+
+  if (doc.type === "press_release") {
+    return "/press-release/" + doc.uid + "/";
+  }
+  if (doc.type === "sweepstakes") {
+    return "/sweepstakes/" + doc.uid + "/";
+  }
+  if (doc.type === "resource") {
+    return "/resource/" + doc.uid + "/";
+  }
+  if (doc.type === "customer") {
+    return "/customers/" + doc.uid + "/";
+  }
+  if (doc.type === "original_xchange_item") {
+    return "/xchange/" + doc.uid + "/";
+  }
+
+  if (doc.type === "livinglens_use_case") {
+    return "/livinglens/product/use-cases/" + doc.uid + "/";
+  }
+  if (doc.type === "livinglens_case_studies") {
+    return "/livinglens/product/use-cases/case-study/" + doc.uid + "/";
+  }
+}
